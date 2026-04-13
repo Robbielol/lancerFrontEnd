31912daa632328309b716c6ccb603db9da03e581
Robbielol <laffanhome@gmail.com>
2026-04-12 22:32:34 -0700

    Fixing to URL god save me.

--- src/api/PlacesApi.js
+++ src/api/PlacesApi.js
@@ -1,6 +1,6 @@
 const LOCAL_API_URL = 'https://api.lancerfind.com';
 
-export const handleSearch = async (businessType, city, distance) => {
+export const handleSearch = async (city, businessType, distance) => {
     const response = await fetch(LOCAL_API_URL+'/api/website/'+city+'/'+businessType+'/'+distance, {
         method: 'GET',
         cache: 'no-cache',
